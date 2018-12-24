01bfc5ce0f5764916948689945a0d37e322b4b4d
林振辉 <315713565@163.com>
2018-12-24 13:35:31 +0800

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# 一个使用 docstrap 主题的 jsdoc 实例
+
+- [实例](https://lin09.github.io/jsdoc-docstrap-demo/doc/index.html)
+
+### 使用
+    yarn global add jsdoc
+    yarn
+    yarn doc
---
+++ src/index.js
@@ -0,0 +1,14 @@
+/**
+ * test
+ */
+var test = 'test'
+
+/**
+ * 两个数求和
+ * @param {number} a 求和值
+ * @param {number} d 求和值
+ * @returns {number} 两个数之和
+ */
+function add (a, d) {
+  return a + d
+}
